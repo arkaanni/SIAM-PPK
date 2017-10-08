249d5a76983385d54719f552ea478007313778c6
Muhammad Abdan Mulia <abdanmulia@gmail.com>
2017-10-08 16:58:13 +0700

    Initial Commit

---
+++ SIAM/SIAM/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace SIAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNIM = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.btnMasuk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(409, 80);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NIM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // tbNIM
+            // 
+            this.tbNIM.Location = new System.Drawing.Point(135, 106);
+            this.tbNIM.Name = "tbNIM";
+            this.tbNIM.Size = new System.Drawing.Size(217, 20);
+            this.tbNIM.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(135, 141);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(217, 20);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // btnMasuk
+            // 
+            this.btnMasuk.Location = new System.Drawing.Point(49, 194);
+            this.btnMasuk.Name = "btnMasuk";
+            this.btnMasuk.Size = new System.Drawing.Size(303, 57);
+            this.btnMasuk.TabIndex = 5;
+            this.btnMasuk.Text = "MASUK";
+            this.btnMasuk.UseVisualStyleBackColor = true;
+            this.btnMasuk.Click += new System.EventHandler(this.btnMasuk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 264);
+            this.Controls.Add(this.btnMasuk);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbNIM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "SIAM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNIM;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button btnMasuk;
+    }
+}
+
---
+++ SIAM/SIAM/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace SIAM
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMasuk_Click(object sender, EventArgs e)
+        {
+            String nim = tbNIM.Text;
+            String password = tbPassword.Text;
+            HttpWebRequest http = (HttpWebRequest)WebRequest.Create("https://tanikita.000webhostapp.com/index.php?nim=155150200111094&password=sandibarunih");
+            WebResponse response = http.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            String content = sr.ReadToEnd();
+            tbHasil.Text = content;
+            JavaScriptSerializer json_serializer = new JavaScriptSerializer();
+            Auth dc = json_serializer.Deserialize<Auth>(content);
+
+            tbHasil.Text = dc.PHPSESSID;
+
+        }
+    }
+    class Auth
+    {
+        public string PHPSESSID { get; set; }
+    }
+}
